3a1c2173a5127de8f2c32ad4d6db79fc87d4005e
Brian Peterson <bpeterson1579@gmail.com>
2021-11-30 09:38:43 -0700

    Allow user to add cards

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, {Component} from 'react';
 import './App.css';
 import Ideas from './Ideas';
+import Form from './Form';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       ideas: [
-        // { id: 1, title: 'Prank Travis', description: 'Stick googly eyes on all his stuff' },
-        // { id: 2, title: 'Make a secret password app', description: 'So you and your rideshare driver can both know neither one of you is lying' },
-        // { id: 3, title: 'Learn a martial art', description: 'To exact vengeance upon my enemies' },
       ],
     }
+  }
+
+  addIdea = (newIdea) => {
+    this.setState({ideas: [...this.state.ideas, newIdea]});
   }
 
   render() {
@@ -19,6 +21,7 @@ class App extends Component {
       <main className='App'>
         <h1>Idea Box</h1>
         {!this.state.ideas.length && <h2>No ideas yet -- add some!</h2>}
+        <Form addIdea={this.addIdea} />
         <Ideas ideas={this.state.ideas} />
       </main>
     )
